804caa23f292fcfd26e2b1fd207b8a548d703893
Muhammed Mijwad <mijuzz007@gmail.com>
2025-05-02 18:11:57 +0530

    csrf

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,6 +112,7 @@ CSRF_TRUSTED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:8000",
+    "http://ec2-44-244-135-39.us-west-2.compute.amazonaws.com"
 ]
 CSRF_COOKIE_SAMESITE = 'Lax'
 CSRF_COOKIE_HTTPONLY = False
